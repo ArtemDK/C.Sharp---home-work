b7d2d12e2550816724f3dab5a55c41e91dd58eb0
Докучаев Артем Игоревич <A.Dokuchaev@cft.ru>
2022-08-09 10:59:56 +0700

    Final

--- DZ-Task4/Program.cs
+++ DZ-Task4/Program.cs
@@ -1,5 +1,4 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
@@ -8,5 +7,9 @@ Console.Write("Введите 3 числа: ");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 int c = Convert.ToInt32(Console.ReadLine());
-int maximal = Math.Max(Math.Max(a, b), c);
-Console.Write($"максимальное число{maximal}");
+int max = a;
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+Console.Write($"максимальное число: {max}");
