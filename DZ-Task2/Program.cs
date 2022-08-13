5a4777237bf9ed60a5817f47e47d5a4e2b6ef3b0
Докучаев Артем Игоревич <A.Dokuchaev@cft.ru>
2022-08-13 18:48:44 +0700

    ДЗ по второму Семинару

--- DZ-Task2/Program.cs
+++ DZ-Task2/Program.cs
@@ -4,7 +4,6 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-
 Console.Write("Введите целое число 1: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите целое число 2: ");
